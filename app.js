9f6c33e87fca956c3fde6762c8037e7775578533
David Ren <david.ren@sap.com>
2016-05-12 16:13:47 +0800

    Add startup nodejs code and test text file.

---
+++ app.js
@@ -0,0 +1,15 @@
+var http = require("http");
+var fs = require("fs");
+
+http.createServer(function(request, response) {
+    // 发送 HTTP 头部
+    // HTTP 状态值： 200 ： OK
+    // 内容类型： text/plain
+    response.writeHead(200, {"Content-Type": "text/plain"});
+
+    var data = fs.readFileSync("input.txt");
+    response.end("Hello World! " + data.toString());
+}).listen(8888);
+
+console.log("Server is running at http://127.0.0.1:8888");
+
